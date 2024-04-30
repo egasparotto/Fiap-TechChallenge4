fad8879e224003579de40240547d4d8dba877b51
Eduardo Gasparotto <gasp.eduardo@gmail.com>
2024-04-30 08:05:55 -0300

    Criada estrutura base

---
+++ FiapReservas.WebAPI/Controllers/TesteController.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace FiapReservas.WebAPI.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class TesteController : ControllerBase
+    {
+        [HttpGet]
+        public string? Teste()
+        {
+            return Environment.GetEnvironmentVariable("NOME");
+        }
+    }
+}
