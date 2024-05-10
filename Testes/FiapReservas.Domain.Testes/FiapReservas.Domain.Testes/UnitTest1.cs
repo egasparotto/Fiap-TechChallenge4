e71fececdad39fe91f31ba2f3fa7161ae4fab688
Maurici Alvarez <maurici.alvarez@gmail.com>
2024-05-10 17:21:32 -0300

    Criando Projeto de Testes

---
+++ Testes/FiapReservas.Domain.Testes/FiapReservas.Domain.Testes/UnitTest1.cs
@@ -0,0 +1,11 @@
+namespace FiapReservas.Domain.Testes
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void Test1()
+        {
+
+        }
+    }
+}
---
+++ Testes/FiapReservas.Domain.Testes/UsersTestes.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FiapReservas.Domain.Testes
+{
+    public class UsersTestes
+    {
+
+        [Fact(DisplayName = "Teste Users Adicionar Novo Usuario")]
+        [Trait("Categoria", "Users Testes")]
+        public void AdicionarUsuario()
+        {
+            //Arrange
+            //new
+
+            //Act
+            //
+
+            //Assert
+
+        }
+    }
+}
